7941e88bcceb6b54d7fd3ba09a2d74598b446607
Juwurason <ajibolasunday7@gmail.com>
2023-01-16 17:18:37 +0100

    third commit

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,7 @@
 import { Session } from 'express-session'
 import { TokenSet } from 'openid-client';
 import { XeroAccessToken, XeroIdToken } from 'xero-node';
-
+ // ////
 declare module 'express-session' {
   interface Session {
     decodedAccessToken: XeroAccessToken;
